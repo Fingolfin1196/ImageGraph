51c5d8ef4e6d6104705f675c799f0a4dd1298479
wucke13 <wucke13@gmail.com>
2020-06-22 17:52:11 +0200

    add nix build script

---
+++ default.nix
@@ -0,0 +1,36 @@
+with import <nixpkgs> {};
+
+
+(overrideCC stdenv clang_9).mkDerivation {
+  name = "st16sim";
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
+
+  cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
+
+  postInstall = ''
+    cp -R ../dependencies $out/include/
+  '';
+
+  buildInputs = [
+    abseil-cpp
+    boost.dev
+    fftw.dev
+    glib.dev
+    gsl
+    imagemagick.dev
+    libexif
+    libgsf.dev
+    openexr.dev
+    orc.dev
+    pcg_c
+    pcre.dev
+    tbb
+    vips
+  ];
+}
